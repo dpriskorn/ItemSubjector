57200c86fb563e09caeada62f807c33f5cb5ad7c
Dennis Priskorn <egils@riseup.net>
2021-09-12 19:11:21 +0200

    Lower max offset

--- helpers/calculations.py
+++ helpers/calculations.py
@@ -5,4 +5,4 @@ import config
 
 def calculate_random_offset():
     # Calculate offset between 0 and
-    config.random_offset = random.randint(0, 100000)
+    config.random_offset = random.randint(0, 10000)
