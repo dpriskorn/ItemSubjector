d8cd779408b0b87897938bf74332b9c0d5b18751
Dennis Priskorn <egils@riseup.net>
2021-09-23 13:16:15 +0200

    More information about pooled WDQS servers and how to file a ticket

--- helpers/console.py
+++ helpers/console.py
@@ -41,7 +41,12 @@ def print_keep_an_eye_on_wdqs_lag():
     console.print("Please keep an eye on the lag of the WDQS cluster here and avoid "
                   "working if it is over a few minutes.\n"
                   "https://grafana.wikimedia.org/d/000000489/wikidata-query-service?"
-                  "orgId=1&viewPanel=8&from=now-30m&to=now&refresh=1d")
+                  "orgId=1&viewPanel=8&from=now-30m&to=now&refresh=1d "
+                  "You can see if any lagging servers are pooled here\n"
+                  "https://config-master.wikimedia.org/pybal/eqiad/wdqs\n"
+                  "If any enabled servers are lagging more than 5-10 minutes "
+                  "you can search phabricator for open tickets to see if the team is on it. "
+                  "If not feel free to create a new ticket like this: https://phabricator.wikimedia.org/T291621")
 
 
 def press_enter_to_start():
