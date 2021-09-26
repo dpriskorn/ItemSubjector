0c4394c53b3fcc9e5c5f15b7815b2b235b6b5152
Dennis Priskorn <egils@riseup.net>
2021-09-26 20:23:47 +0200

    Fix garbled message

--- tasks.py
+++ tasks.py
@@ -8,17 +8,17 @@ from models.task import Task
 tasks = [
     Task(
         best_practice_information=(
-            "(done in Q108532542 and add that as main subject and "
-            "E.g. when searching for 'cancer screening' in Wikidata "
-            "In this case it is preferred to first create that item "
             "When adding QID main subjects please try to first "
-            "and find/create items as specific as possible.\n"
-            "avoid the more general 'cancer screening' until all "
             "educate yourself about the subarea of science a little "
-            "perhaps no item for this yet.\n"
-            "scientific articles but there is "
-            "sub forms of screening have been matched."
+            "and find/create items as specific as possible.\n"
+            "E.g. when searching for 'cancer screening' in Wikidata "
             "we find 'gastric cancer screening' in labels of "
+            "scientific articles but there is "
+            "perhaps no item for this yet.\n"
+            "In this case it is preferred to first create that item "
+            "(done in Q108532542 and add that as main subject and "
+            "avoid the more general 'cancer screening' until all "
+            "sub forms of screening have been matched."
         ),
         items=ScholarlyArticleItems(),
         label="Work on main subject on scholarly articles (English)",
