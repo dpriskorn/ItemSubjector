bcc532a97a11c29d49d52b91822c4fe4f7c7d0da
Dennis Priskorn <egils@riseup.net>
2021-09-27 09:50:02 +0200

    Remove dead code

--- src/__init__.py
+++ src/__init__.py
@@ -214,16 +214,6 @@ def setup_argparse_and_return_args():
     return parser.parse_args()
 
 
-# def get_main_subjects_from_file() -> List[str]:
-#     # read the data file
-#     file_path = "data/main_subjects.pkl"
-#     main_subjects_path = f"{os.getcwd()}/{file_path}"
-#     with open(main_subjects_path) as file:
-#         lines = file.readlines()
-#         main_subjects = [line.rstrip() for line in lines]
-#     return main_subjects
-
-
 def get_validated_random_subjects(args: argparse.Namespace = None,
                                   main_subjects: List[str] = None):
     if args is None:
