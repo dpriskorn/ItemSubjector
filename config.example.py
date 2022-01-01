da2563b510682afbf783c564bfc57094fa19a4e3
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-01 13:17:24 +0100

    config.example.py: Use generic username

--- config.example.py
+++ config.example.py
@@ -7,11 +7,11 @@ username = ""
 password = ""
 
 # Global settings
+wiki_user = "User:Username"  # Change this to your username
 logging.basicConfig(level=logging.WARNING)
 version = "0.2"  # Don't touch this.
 wd_prefix = "http://www.wikidata.org/entity/"
 endpoint = "https://query.wikidata.org/sparql"
-wiki_user = "User:So9q"  # Change this to your username
 user_agent = f"ItemSubjector/{version} (https://github.com/dpriskorn/ItemSubjector), {wiki_user}"
 tool_url = "https://github.com/dpriskorn/ItemSubjector"
 tool_wikipage = "Wikidata:Tools/ItemSubjector"
