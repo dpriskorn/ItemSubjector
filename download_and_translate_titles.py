2ed31c5bbc785db9341649a06cc097692f96ec02
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-01-01 20:31:00 +0100

    WIP download_and_translate_titles.py for better detecting the subjects of journals in WD

--- download_and_translate_titles.py
+++ download_and_translate_titles.py
@@ -1,10 +1,15 @@
 # download all titles missing P921
-from langdetect import detect
+import logging
+
+from langdetect import detect, LangDetectException
 from wikibaseintegrator import wbi_config
 from wikibaseintegrator.wbi_helpers import execute_sparql_query
 
 import config
 from src import EntityID, console
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 wbi_config.config['USER_AGENT'] = config.user_agent
 results = execute_sparql_query("""
@@ -20,14 +25,31 @@ WHERE
   filter(!(lang(?title) = "und"))
 }}
 """)
+ignore_list = ["ca", "ro", "no"]
 if results is not None:
+    count = 0
+    red_count = 0
+    total = len(results["results"]["bindings"])
     for result in results["results"]["bindings"]:
         item = str(EntityID(result["item"]["value"]))
         title = result["title"]["value"]
         language = result["title"]["xml:lang"]
-        detected_language = detect(title)
-        if language != detected_language:
-            console.print(f"[red]{item} {title} {language} {detected_language}[red]")
+        try:
+            detected_language = detect(title)
+        except LangDetectException:
+            logger.error(f"Could not detect language for {title}")
+            detected_language = None
+        if len(title.split(" ")) < 4:
+            short_title = True
         else:
-            console.print(f"[green]{item} {title} {language} {detected_language}[green]")
+            short_title = False
+        if language != detected_language and short_title is False and detected_language not in ignore_list:
+            console.print(f"[red]{item} {title} {language} {detected_language}[red]")
+            red_count += 1
+        else:
+            pass
+            # console.print(f"[green]{item} {title} {language} {detected_language}[green]")
         #exit(0)
+        count += 1
+        if count % 1000 == 0:
+            logger.info(f"{count}/{red_count}/{total}")
