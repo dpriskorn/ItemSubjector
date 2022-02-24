4302f96a8606552b0b6b9c25f5632f2413c22801
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-24 12:24:34 +0100

    Add setuptools configuration and prepare 0.3-alpha0

--- config.example.py
+++ config.example.py
@@ -11,10 +11,11 @@ password = ""
 loglevel = logging.WARNING
 wiki_user = "User:Username"  # Change this to your username
 list_of_allowed_aliases: List[str] = []  # Add elements like this ["API"]
-version = "0.2"  # Don't touch this.
 wd_prefix = "http://www.wikidata.org/entity/"
 endpoint = "https://query.wikidata.org/sparql"
-user_agent = f"ItemSubjector/{version} (https://github.com/dpriskorn/ItemSubjector), {wiki_user}"
+user_agent = (
+    f"ItemSubjector/{version} (https://github.com/dpriskorn/ItemSubjector), {wiki_user}"
+)
 tool_url = "https://github.com/dpriskorn/ItemSubjector"
 tool_wikipage = "Wikidata:Tools/ItemSubjector"
 login_instance = None
