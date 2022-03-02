bc9721efbdb14261b680611af89576be2f542f3e
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-03-02 07:56:45 +0100

    + more blocked items

--- config/items.py
+++ config/items.py
@@ -6,5 +6,11 @@ from typing import List
 list_of_allowed_aliases: List[str] = []  # Add elements like this ["API"]
 
 # Scholarly items settings
-blocklist_for_scholarly_items: List[str] = ["Q28196260", "Q27863114"]
-no_alias_for_scholarly_items: List[str] = ["Q407541", "Q423930"]
+blocklist_for_scholarly_items: List[str] = [
+    "Q28196260",
+    "Q27863114",
+    "Q28196260",
+    "Q28196266",  # iodine
+    "Q27863114",  # testosterone
+]
+no_alias_for_scholarly_items: List[str] = ["Q407541", "Q423930", "Q502327"]
