9bcc8565edbee9fd520d4c80519ff7ba849971f6
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-10-03 20:25:03 +0200

    Update classes.puml and comment out dead code.

--- src/models/wikimedia/wikidata/foreign_id.py
+++ src/models/wikimedia/wikidata/foreign_id.py
@@ -1,23 +1,23 @@
-from typing import Optional
-
-from src.models.wikimedia.wikidata.entiyt_id import EntityId
-
-
-class ForeignID:
-    id: Optional[str]
-    property: Optional[str]  # This is the property with type ExternalId
-    source_item_id: Optional[str]  # This is the Q-main_subject_item for the source
-
-    def __init__(
-        self,
-        id: Optional[str] = None,
-        property: Optional[str] = None,
-        source_item_id: Optional[str] = None,
-    ):
-        self.id = id
-        if property is None:
-            raise ValueError("property was None")
-        self.property = str(EntityId(property))
-        if source_item_id is None:
-            raise ValueError("source_item_id was None")
-        self.source_item_id = str(EntityId(source_item_id))
+# from typing import Optional
+#
+# from src.models.wikimedia.wikidata.entiyt_id import EntityId
+#
+#
+# class ForeignID:
+#     id: Optional[str]
+#     property: Optional[str]  # This is the property with type ExternalId
+#     source_item_id: Optional[str]  # This is the Q-main_subject_item for the source
+#
+#     def __init__(
+#         self,
+#         id: Optional[str] = None,
+#         property: Optional[str] = None,
+#         source_item_id: Optional[str] = None,
+#     ):
+#         self.id = id
+#         if property is None:
+#             raise ValueError("property was None")
+#         self.property = str(EntityId(property))
+#         if source_item_id is None:
+#             raise ValueError("source_item_id was None")
+#         self.source_item_id = str(EntityId(source_item_id))
